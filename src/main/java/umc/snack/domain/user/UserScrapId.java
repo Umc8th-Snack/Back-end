d07855a6a67ca44e8ed3207bcfd4f4075ffa5d84
sungchaewon <scw226@sookmyung.ac.kr>
2025-07-11 14:34:34 +0900

    feat(add controller) : scrap controller 추가

--- src/main/java/umc/snack/domain/user/UserScrapId.java
+++ src/main/java/umc/snack/domain/user/UserScrapId.java
@@ -1,3 +1,8 @@
+/*[사용 중단됨] 복합키 매핑을 위한 식별자 클래스입니다.
+원래 UserScrap 엔티티는 scrapId, articleId, userId를 합친 복합키(@IdClass)를 사용했으나,
+유지 보수와 단순화를 위해 scrapId를 단일 기본키로 변경하였습니다.
+이에 따라 이 클래스는 더 이상 사용되지 않으며, 추후 완전히 삭제될 예정입니다.
+(복구가 필요할 경우 Git 기록에서 확인 가능)
 package umc.snack.domain.user;
 
 import lombok.*;
@@ -28,4 +33,4 @@ public class UserScrapId implements Serializable {
     public int hashCode() {
         return Objects.hash(scrapId, articleId, userId);
     }
-}
+}*/
