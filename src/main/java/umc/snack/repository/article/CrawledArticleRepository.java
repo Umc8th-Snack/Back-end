ed6764d3e6a289449845afe8860df1662de0ac0c
이지원 <daggero@naver.com>
2025-07-18 20:39:02 +0900

    [test] Gemini API 응답 생성

--- src/main/java/umc/snack/repository/article/CrawledArticleRepository.java
+++ src/main/java/umc/snack/repository/article/CrawledArticleRepository.java
@@ -3,12 +3,14 @@ package umc.snack.repository.article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.snack.domain.article.entity.CrawledArticle;
 
+import java.util.List;
+
 public interface CrawledArticleRepository extends JpaRepository<CrawledArticle, Long> {
 
     boolean existsByArticleUrl(String articleUrl); // 중복 방지용
 
     long countByStatus(CrawledArticle.Status status); // 상태별 개수 세기용 (PROCESSED, FAILED 등)
 
-
+    List<CrawledArticle> findByStatus(CrawledArticle.Status status);
 
 }
