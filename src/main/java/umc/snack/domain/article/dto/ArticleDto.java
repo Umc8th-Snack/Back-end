216f53d8712d6be1db91d9ca16047e60f451cdd1
sungchaewon <scw226@sookmyung.ac.kr>
2025-07-21 19:06:04 +0900

    fix(feat/scrap) : ArticleDto에서 viewcount와 imageUrl 다시 추가

--- src/main/java/umc/snack/domain/article/dto/ArticleDto.java
+++ src/main/java/umc/snack/domain/article/dto/ArticleDto.java
@@ -10,6 +10,8 @@ public class ArticleDto {
     private String title;
     private String summary;
     private String publishedAt;
+    private String imageUrl;
+    private Integer viewCount;
     private String articleUrl;    // 외부 네이버 뉴스 URL
     private String snackUrl;      // "/articles/{id}" (스낵 서비스 내 상세 페이지 경로)
     private String category;  // 카테고리 이름
