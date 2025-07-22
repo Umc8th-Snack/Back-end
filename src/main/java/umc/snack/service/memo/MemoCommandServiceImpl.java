4f69624a7a514cdeca84eabd1a51387e06a9d3d7
PADO <sanhobox@gmail.com>
2025-07-23 04:02:34 +0900

    code rabbit 리뷰 반영

--- src/main/java/umc/snack/service/memo/MemoCommandServiceImpl.java
+++ src/main/java/umc/snack/service/memo/MemoCommandServiceImpl.java
@@ -1,6 +1,5 @@
 package umc.snack.service.memo;
 
-import ch.qos.logback.core.status.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,13 +23,21 @@ public class MemoCommandServiceImpl implements MemoCommandService {
 
     @Override
     public Memo createMemo(Long articleId, MemoRequestDto.CreateDto request) {
+        // JWT 인증 토큰을 발급하는 시스템이 아직 구현되어있지 않아서 구현한 임시코드입니다.
         User currentUser = userRepository.findById(1L)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_2622));
+        /* 인증 시스템 구현 완료 되면 아래 코드로 수정 예정입니다.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long currentUserId = ((UserPrincipal) authentication.getPrincipal()).getId();
+        User currentUser = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_2622));
+        */
         Article article = articleRepository.findById(articleId)
                 .orElseThrow(() -> new CustomException(ErrorCode.MEMO_8605));
         Memo newMemo = Memo.builder()
                 .content(request.getContent())
                 .article(article)
+                .user(currentUser)
                 .build();
 
         return memoRepository.save(newMemo);
