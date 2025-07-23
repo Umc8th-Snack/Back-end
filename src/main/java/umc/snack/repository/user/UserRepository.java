83268f0e1416dedb3fd83ea5c4bb7f331cdefb70
이지원 <daggero@naver.com>
2025-07-23 16:49:02 +0900

    [feat] CustomUserDetails 생성

--- src/main/java/umc/snack/repository/user/UserRepository.java
+++ src/main/java/umc/snack/repository/user/UserRepository.java
@@ -6,4 +6,6 @@ import umc.snack.domain.user.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
+    User findByEmail(String email);
+    User findByNickname(String nickname);
 }
