94a0a3ac80534a2a3c1e72a2627b8e18c376aaa8
weeeeestern <liz0824@naver.com>
2025-07-28 12:12:42 +0900

    [fix] 1차 수정

--- src/main/java/umc/snack/controller/share/ShareController.java
+++ src/main/java/umc/snack/controller/share/ShareController.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import umc.snack.common.config.security.CustomUserDetails;
 import umc.snack.common.dto.ApiResponse; // 공통 응답 DTO 임포트
 import umc.snack.domain.share.dto.ShareResultDto; // POST 응답 result용 DTO 임포트 (새로 정의)
 import umc.snack.domain.share.dto.SharedArticleContentDto; // GET 응답 result용 DTO 임포트 (새로 정의)
@@ -25,9 +27,10 @@ public class ShareController {
             description = "특정 기사의 공유용 UUID를 생성합니다. 버튼 누르면 바로 복사되도록."
     )
     @PostMapping("/articles/{articleId}/share")
-    public ResponseEntity<ApiResponse<ShareResultDto>> createShare(@PathVariable Long articleId) {
-        // TODO: 추후 인증 연동 시 userId → SecurityContext 등에서 가져와서 전달
-        ShareResultDto result = shareService.createShareLink(articleId, null);
+    public ResponseEntity<ApiResponse<ShareResultDto>> createShare(@PathVariable Long articleId,
+                                                                   @AuthenticationPrincipal CustomUserDetails userDetails){
+        // Todo : jwt 토큰 로직 완성 되면.. Long userId = userDetails.getUser().getUserId();
+        ShareResultDto result = shareService.createShareLink(articleId, userId);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.onSuccess("SHARE_6501", "공유 링크 생성 성공", result));
     }
