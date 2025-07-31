415cfa5c58dc987d9315842005dbeeb7d4814fba
PADO <sanhobox@gmail.com>
2025-08-01 06:32:41 +0900

    feat (memo) : 메모 목록 조회 구현

--- src/main/java/umc/snack/converter/memo/MemoConverter.java
+++ src/main/java/umc/snack/converter/memo/MemoConverter.java
@@ -1,7 +1,11 @@
 package umc.snack.converter.memo;
 
+import org.springframework.data.domain.Page;
 import umc.snack.domain.memo.dto.MemoResponseDto;
 import umc.snack.domain.memo.entity.Memo;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MemoConverter {
     public static MemoResponseDto.CreateResultDto toCreateResultDto(Memo memo) {
@@ -15,6 +19,29 @@ public class MemoConverter {
         return MemoResponseDto.UpdateResultDto.builder()
                 .memoId(memo.getMemoId())
                 .content(memo.getContent())
+                .build();
+    }
+
+    public static MemoResponseDto.MemoInfo toMemoInfo(Memo memo) {
+        return MemoResponseDto.MemoInfo.builder()
+                .memoId(memo.getMemoId())
+                .content(memo.getContent())
+                .createdAt(memo.getCreatedAt())
+                .articleUrl(memo.getArticle().getArticleUrl())
+                .build();
+    }
+
+    public static MemoResponseDto.MemoListDto toMemoListDto(Page<Memo> memoPage) {
+        List<MemoResponseDto.MemoInfo> memoInfos = memoPage.getContent().stream()
+                .map(MemoConverter::toMemoInfo)
+                .collect(Collectors.toList());
+
+        return MemoResponseDto.MemoListDto.builder()
+                .memos(memoInfos)
+                .page(memoPage.getNumber())
+                .size(memoPage.getSize())
+                .totalPages(memoPage.getTotalPages())
+                .totalElements(memoPage.getTotalElements())
                 .build();
     }
 }
