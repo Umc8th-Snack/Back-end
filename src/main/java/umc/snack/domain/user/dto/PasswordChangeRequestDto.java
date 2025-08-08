c6d803a175ed2448fb9de92a9b3fa2259262b5a2
이지원 <daggero@naver.com>
2025-08-09 03:21:01 +0900

    [feat] 비밀번호 변경 DTO 생성

---
+++ src/main/java/umc/snack/domain/user/dto/PasswordChangeRequestDto.java
@@ -0,0 +1,18 @@
+package umc.snack.domain.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PasswordChangeRequestDto {
+
+    @NotBlank(message = "현재 비밀번호를 입력해주세요.")
+    private String currentPassword;
+
+    @NotBlank(message = "새 비밀번호를 입력해주세요.")
+    @Size(min = 8, max = 20, message = "비밀번호는 2자 이상 12자 이하이어야 합니다.")
+    private String newPassword;
+
+    @NotBlank(message = "새 비밀번호 확인을 입력해주세요.")
+    private String confirmPassword;
+
+}
