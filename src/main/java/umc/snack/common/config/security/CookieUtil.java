db14dcafde7355f66d46febaca65feff79958e72
이지원 <daggero@naver.com>
2025-08-13 04:45:10 +0900

    [test] createCookie 시간 변경

--- src/main/java/umc/snack/common/config/security/CookieUtil.java
+++ src/main/java/umc/snack/common/config/security/CookieUtil.java
@@ -6,7 +6,8 @@ public class CookieUtil {
     // 쿠키 생성 메서드
     public static Cookie createCookie(String key, String value) {
         Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(24 * 60 * 60);
+//        cookie.setMaxAge(24 * 60 * 60);
+        cookie.setMaxAge(10 * 60);
         cookie.setHttpOnly(true);
 //        cookie.setSecure(true);  // 배포 환경에서만 활성화
         cookie.setPath("/");
