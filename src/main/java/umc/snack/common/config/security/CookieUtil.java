95b8c7c1de7ae239c66d074fc894bc685772e6d9
이지원 <daggero@naver.com>
2025-08-20 23:23:53 +0900

    [fix] cookieUtil 클래스 만료기간 1일로 변경

--- src/main/java/umc/snack/common/config/security/CookieUtil.java
+++ src/main/java/umc/snack/common/config/security/CookieUtil.java
@@ -8,7 +8,7 @@ public class CookieUtil {
     // Cross-Domain 환경을 지원하는 쿠키 생성 메서드 (ResponseCookie 사용)
     public static void createCookie(String key, String value, HttpServletResponse response) {
         ResponseCookie cookie = ResponseCookie.from(key, value)
-                .maxAge(3 * 10 * 60)  // 개발 단계: 30분
+                .maxAge(24 * 60 * 60)  // 1일
                 .httpOnly(true)
                 .secure(true)  // HTTPS 환경에서만 전송
                 .path("/")
